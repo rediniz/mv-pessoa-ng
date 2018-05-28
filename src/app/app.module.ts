11f485e96f3baa41c11107fe13bd198c07c61d9c
renato.diniz <renato.diniz@freelifesaude.com.br>
2018-05-28 11:23:10 -0300

    classes de serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { routing } from './../app.routes';
+
+import { ConfigService } from './services/config.service';
+import { PessoaService } from './services/pessoa.service';
+import { Pessoa } from './services/pessoa';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [ConfigService, PessoaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
